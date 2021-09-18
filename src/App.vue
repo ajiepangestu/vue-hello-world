e88bb48ddc89e9b7d54979864c83c7eacd508839
Ajie Pangestu <paangestu@gmail.com>
2021-09-18 11:25:01 +0700

    add lifecyle hook

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,14 @@
 <template>
     <div>
-        <button @click="decrement">-</button>
-        {{ number }}
-        <button @click="increment">+</button>
+        <Post />
     </div>
 </template>
 
 <script>
-import { reactive, toRefs } from 'vue'
-import useCounter from './helpers/counter'
+import Post from './components/Post'
 export default {
-    setup () {
-        const state = reactive({
-            number: 0
-        })
-        const { decrement, increment } = useCounter(state)
-        return {
-            ...toRefs(state), decrement, increment
-        }
+    components: {
+        Post
     }
 }
 </script>
---
+++ src/components/Post.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <ul>
+            <li
+                v-for="post in posts"
+                :key="post.id"
+            >
+                {{ post.title }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import { onMounted, ref } from 'vue'
+import axios from 'axios'
+export default {
+    setup () {
+        const posts = ref([])
+        const getPosts = async () => {
+            const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+            posts.value = response.data
+        }
+        onMounted(() => {
+            getPosts()
+        })
+        return {
+            posts
+        }
+    }
+}
+</script>
