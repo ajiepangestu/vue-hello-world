f2e506d3c6e2d51d98a078f9ce2849836e598378
Ajie Pangestu <paangestu@gmail.com>
2021-09-18 10:13:40 +0700

    use reactive, add toRefs when destructuring reactive, fix reactivity remove on primitive data

--- src/App.vue
+++ src/App.vue
@@ -1,80 +1,26 @@
 <template>
     <div>
-        <div>{{ number }}</div>
-        <div>
-            {{ form.title }}
-            {{ form.body }}
-        </div>
-        <div>
-            <div>{{ state.user.name }}</div>
-            <div>{{ state.user.email }}</div>
-        </div>
-        <div>
-            <div>{{ state.post.title}}</div>
-            <div>{{ state.post.body }}</div>
-        </div>
+        <div>Loading: {{ loading }}</div>
+        <div>My Name is {{ user.name }}</div>
     </div>
 </template>
 
 <script>
-import { ref, reactive } from 'vue'
+import { reactive, toRefs } from 'vue'
 export default {
-    // Options API
-    // data () {
-    //     return {
-    //         number: 0
-    //     }
-    // },
-    // mounted () {
-    //     setInterval(() => {
-    //         this.number++
-    //     }, 1000)
-    // }
-
-    // Composition API
     setup () {
-        // different reactive and ref
-        // ref declaration, ref used for primitive data (boolean, int, string)
-        const number = ref(0)
-        // reactive declaration, reactive used for complex data(array, object)
-        const form = reactive({
-            title: 'First Title',
-            body: 'The First Body'
-        })
-        // the usual way to use reactive
         const state = reactive({
-            // cannot use primitive data in there because with DESTRUCTURING object can make removed reactivity
-            // so must use ref
-            // loading: false
+            loading: false,
             user: {
-                name: 'Ajie Pangestu',
-                email: 'paangestu@gmail.com'
-            },
-            post: {
-                title: 'First Post',
-                body: 'First Body'
+                name: 'Ajie Pangestu'
             }
         })
-        // the usual way to use ref
-        const loading = ref(false)
-        setInterval(() => {
-            // ref, must use '.value' for change value of variable
-            number.value++
-            loading.value = true
-            // reactive
-            form.title = 'Second Title'
+        setTimeout(() => {
+            state.loading = true
             state.user.name = 'Ajie'
-            state.post.title = 'Second Title'
-        }, 1000)
-
-        // if use state reactive
-        // return {
-        //     state
-        // }
-
+        }, 2000)
         return {
-            // Destructuring object reactive can remove reactivity of object
-            number, form, ...state
+            ...toRefs(state)
         }
     }
 }
