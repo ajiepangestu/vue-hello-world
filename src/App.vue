f39e96ade1058955f2cc4a3df659126326bbfe13
Ajie Pangestu <paangestu@gmail.com>
2021-09-19 09:32:20 +0700

    add vue router

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,32 @@
 <template>
     <div>
-        <Post />
+        <div id="nav">
+            <router-link to="/">Home</router-link> |
+            <router-link to="/about">About</router-link>
+        </div>
+        <router-view />
     </div>
 </template>
 
-<script>
-import Post from './components/Post'
-export default {
-    components: {
-        Post
+<style lang="scss">
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+}
+
+#nav {
+    padding: 30px;
+
+    a {
+        font-weight: bold;
+        color: #2c3e50;
+
+        &.router-link-exact-active {
+            color: #42b983;
+        }
     }
 }
-</script>
+</style>
